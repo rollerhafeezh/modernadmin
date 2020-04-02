e2804e45041f25933c712fc0ae7473d648756361
Hafidz Sanjaya <rollerhafeezh@gmail.com>
2020-04-02 11:52:57 +0700

    Update basic-line.js

--- js/scripts/charts/echarts/line-area/basic-line.js
+++ js/scripts/charts/echarts/line-area/basic-line.js
@@ -18,7 +18,7 @@ $(window).on("load", function(){
 
     require.config({
         paths: {
-            echarts: '../../../app-assets/vendors/js/charts/echarts'
+            echarts: '../../../vendors/js/charts/echarts'
         }
     });
 
